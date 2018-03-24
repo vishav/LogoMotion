d05720f4af8e0a5fcd005355b12ab7f8996e7c3f
vishav <vishavpandhi@gmail.com>
2018-03-24 11:36:34 -0400

    Initial commit

---
+++ app/src/main/java/com/fsu/android/logomotion/LogoMotionActivity.java
@@ -0,0 +1,13 @@
+package com.fsu.android.logomotion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogoMotionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo_motion);
+    }
+}
